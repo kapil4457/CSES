7eb3e455a1a12381ea1ee4c715f7c788474a4c43
Kapil Soni <kapilsoni54768161@gmail.com>
2022-08-31 10:54:44 +0530

    .

--- permutation.cpp
+++ permutation.cpp
@@ -1,7 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+void f(int n)
+{
+    if (n == 1)
+    {
+        cout << 1 << endl;
+        return;
+    }
+
+    if (n < 4)
+    {
+        cout << "NO SOLUTION" << endl;
+        return;
+    }
+
+    int even = 2;
+    while (even <= n)
+    {
+        cout << even << " ";
+        even += 2;
+    }
+
+    int odd = 1;
+    while (odd <= n)
+    {
+        cout << odd << " ";
+        odd += 2;
+    }
+    cout << endl;
+}
 int main()
 {
-
+    int n;
+    cin >> n;
+    f(n);
     return 0;
 }
